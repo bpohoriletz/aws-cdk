8a0746afc3d957fdc6968b6ec8f4fbd0786f6a80
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2023-04-11 20:47:43 +0300

    Define resources

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
 export interface HitCounterProps {
@@ -8,9 +9,23 @@ export interface HitCounterProps {
 }
 
 export class HitCounter extends Construct {
+  public readonly handler: lambda.Function;
+
   constructor(scope: Construct, id: string, props: HitCounterProps) {
     super(scope, id);
 
-    // TODO
+    const table = new dynamodb.Table(this, 'Hits', {
+      partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
+    });
+
+    this.handler = new lambda.Function(this, 'HitCounterHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'hitcounter.handler',
+      code: lambda.Code.fromAsset('lambda'),
+      environment: {
+        DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+        HITS_TABLE_NAME: props.downstream.tableName
+      }
+    });
   }
 }
